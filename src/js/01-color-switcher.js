0ca9cd83b357e32dd4e8c6c3c78f23ac7214f1b0
SerhiiDubovets <Sergio.d@bigmir.net>
2022-05-11 23:49:10 +0300

    задание 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onColor);
+stopBtn.addEventListener('click', offColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onColor() {
+  timerId = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+}
+function offColor() {
+  clearInterval(timerId);
+  stopBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
+}
