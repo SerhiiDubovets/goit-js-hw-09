0cb71e9fdf5f2246bbf387479e4f5a29fd462e6e
SerhiiDubovets <Sergio.d@bigmir.net>
2022-05-16 10:45:05 +0300

    дз

---
+++ src/js/02-timer.js
@@ -0,0 +1,76 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+
+let selected;
+let intervalId = null;
+
+refs.startBtn.setAttribute('disabled', true);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selected = selectedDates[0];
+    if (selected < options.defaultDate) {
+      alert('Please choose a date in the future');
+      return;
+    }
+    refs.startBtn.removeAttribute('disabled');
+    console.log(selected);
+  },
+};
+
+const fp = flatpickr(refs.input, options);
+
+function getTime() {
+  let interval = selected - Date.now();
+  if (selected > Date.now()) {
+    onTimeClock(convertMs(interval));
+  } else {
+    clearInterval(intervalId);
+  }
+}
+
+refs.startBtn.addEventListener('click', () => {
+  intervalId = setInterval(getTime, 1000);
+});
+
+function onTimeClock({ days, hours, minutes, seconds }) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.minutes.textContent = minutes;
+  refs.seconds.textContent = seconds;
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const refs = {
+  delayEl: document.querySelector('[name="delay"]'),
+  stepEl: document.querySelector('[name="step"]'),
+  amountEl: document.querySelector('[name="amount"]'),
+  submitBtm: document.querySelector('[type="submit"]'),
+};
+
+refs.submitBtm.addEventListener('click', qq);
+function qq(e) {
+  e.preventDefault();
+  let delay = Number(refs.delayEl.value);
+  let step = Number(refs.stepEl.value);
+  for (let index = 1; index <= refs.amountEl.value; index++) {
+    let position = index;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delay += step;
   }
+}
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
